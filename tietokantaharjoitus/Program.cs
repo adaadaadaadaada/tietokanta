b176c02c4b99e693ded7b07353cf469f65074e5b
ada <s2101097@edu.bc.fi>
2023-08-18 12:52:47 +0300

    valikkoa ja varastonhallintaa
    alotin niitä tekeen

--- tietokantaharjoitus/Program.cs
+++ tietokantaharjoitus/Program.cs
@@ -1,1 +1,16 @@
-﻿// valikko, toiminnallisuus ja muut tänne
+﻿using tietokantaharjoitus;
+// valikko, toiminnallisuus ja muut tänne
+
+static void Valikko()
+{
+    Console.WriteLine("VARASTONHALLINTA");
+    Console.WriteLine("1 - Lisää uusi tuote");
+    Console.WriteLine("2 - Poista tuote");
+    Console.WriteLine("3 - Tulosta eri tuotteiden määrät");
+    Console.WriteLine("4 - Tulosta kaikki tuotteet");
+    Console.WriteLine("5 - Muokkaa tuotenimeä");
+    Console.WriteLine("0 - Lopeta sovellus");
+    Console.WriteLine(" ");
+    Console.WriteLine("Valintasi on: ");
+    string userinput = Console.ReadLine();
+}
