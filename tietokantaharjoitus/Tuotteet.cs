30f66c51837fb86e5d0a2770cf7348aa4c327a11
Onni <s2101095@edu.bc.fi>
2023-08-18 12:53:27 +0300

    tuote.cs
    tuote.cs ja tuotteet.cs

---
+++ tietokantaharjoitus/Tuotteet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tietokanta;
+
+
+
+namespace Tuotteet
+{
+    public class Tuotteet
+    {
+        [Key]
+        public string? Kirves { get; set; }
+        public string? Abc { get; set; }
+        public string? Def { get; set; }
+        public string? Ghi { get; set; }
+    }
+}
